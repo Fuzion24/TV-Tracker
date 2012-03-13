30d2d414a5b32821085be1b1d66576f672c5bde7
Ryan Welton <rwelton24@gmail.com>
2012-03-12 21:22:35 -0400

    Added routes for users shows

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,4 @@
+class UserController < ApplicationController
+  def shows
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TvTracker::Application.routes.draw do
   root :to => 'home#index'
-
+  match "/my_shows" => "user#shows"
   resources :tv_episodes
   resources :tv_shows
 end
