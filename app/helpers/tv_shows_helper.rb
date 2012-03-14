86602033838fa39a232883b15a4c3e8f504b8427
Ryan Welton <rwelton24@gmail.com>
2012-03-13 22:46:44 -0400

    Fixed up default icon

--- app/helpers/tv_shows_helper.rb
+++ app/helpers/tv_shows_helper.rb
@@ -1,10 +1,19 @@
 module TvShowsHelper
   def self.last_air_date(tv_show)
-  	tv_show.tv_episodes.limit(1).where("airdate <= ?", '3/20/2012').first
-  	return "03/12/2012"
+  	prev_episode = tv_show.tv_episodes.limit(1).where("airdate <= ?", '3/20/2012').first
+  	if !prev_episode.nil?
+  		return prev_episode.airdate
+  	else
+  		return "No previous episodes"
+  	end
   end
 
   def self.next_air_date(tv_show)
-  	return "03/19/2012"
+  	next_episode = tv_show.tv_episodes.limit(1).where("airdate <= ?", '3/20/2012').first
+  	if !next_episode.nil?
+  		return next_episode.airdate
+  	else
+  		return "No scheduled episodes"
+  	end
   end
 end
