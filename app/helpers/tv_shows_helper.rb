cafc16fd8650901d9c51b3378aaf609584091dce
Ryan Welton <rwelton24@gmail.com>
2012-03-13 22:28:55 -0400

    refactored logic to view helpers

--- app/helpers/tv_shows_helper.rb
+++ app/helpers/tv_shows_helper.rb
@@ -1,2 +1,10 @@
 module TvShowsHelper
+  def self.last_air_date(tv_show)
+  	tv_show.tv_episodes.limit(1).where("airdate <= ?", '3/20/2012').first
+  	return "03/12/2012"
+  end
+
+  def self.next_air_date(tv_show)
+  	return "03/19/2012"
+  end
 end
