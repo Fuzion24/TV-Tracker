7ce25deb13986f80c8dee750e39498feed245513
Ryan Welton <rwelton24@gmail.com>
2012-03-14 00:52:35 -0400

    Did stuff

--- app/helpers/tv_shows_helper.rb
+++ app/helpers/tv_shows_helper.rb
@@ -1,6 +1,6 @@
 module TvShowsHelper
   def self.last_air_date(tv_show)
-  	prev_episode = tv_show.tv_episodes.limit(1).where("airdate <= ?", '3/20/2012').first
+  	prev_episode = tv_show.tv_episodes.limit(1).order("airdate desc").where("airdate <= ?", Date.today.strftime("%F")).first
   	if !prev_episode.nil?
   		return prev_episode.airdate
   	else
@@ -9,7 +9,7 @@ module TvShowsHelper
   end
 
   def self.next_air_date(tv_show)
-  	next_episode = tv_show.tv_episodes.limit(1).where("airdate <= ?", '3/20/2012').first
+  	next_episode = tv_show.tv_episodes.limit(1).order("airdate").where("airdate >= ?", Date.today.strftime("%F")).first
   	if !next_episode.nil?
   		return next_episode.airdate
   	else
