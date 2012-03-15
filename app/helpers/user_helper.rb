31bc3bbd613d3bbadce7ede1881fcdc4e6107c93
Ryan Welton <rwelton24@gmail.com>
2012-03-14 21:57:29 -0400

    Added weekview and cleaned up output a bit

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,7 +4,14 @@ module UserHelper
       tv_show.tv_episodes.where("airdate = '#{date}'").all
    }.flatten
   end
-  def self.showdate(offset)
-        (Date.today + offset).strftime('%A %F')
+
+  def self.day_of_week(offset)
+  	target_date = (Date.today + offset)
+  	target_date.strftime("%A")
+  end
+
+  def self.numeric_date(offset)
+	target_date = (Date.today + offset)
+  	target_date.strftime("%D")
   end 
 end
