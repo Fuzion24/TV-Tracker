31b7dd34a00db1c96969ec80d8cc03c39dd15a92
Akamatsu <logan.akamatsu@mobiledefense.com>
2012-03-24 17:48:24 -0400

    refactored user -> users to satisfy rails magic

--- app/controllers/user_controller.rb
+++
@@ -1,6 +0,0 @@
-class UserController < ApplicationController
-  def shows
-  	@user_episodes = User.first.tv_episodes
-    @tv_shows = TvShow.all
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+        acts_as_authentic do |c|
+          c.login_field = :handle
+        end
+
 	has_many :user_episodes
 	has_many :user_shows
 	has_many :tv_episodes, 		:through => :user_episodes
